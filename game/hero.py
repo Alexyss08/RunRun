be93d4ae2b841efebdebb9c8902cd93e69e059c4
Aleix Perez <08perezaleix@gmail.com>
2025-03-14 19:23:18 +0100

    test 6

--- game/hero.py
+++ game/hero.py
@@ -38,7 +38,7 @@ def make_terrain():
 class Hero(Sprite):
     def __init__(self):
         super().__init__()
-        self.sprite_sheet_idle = pygame.image.load("sprites/monatronica/PNG/Unarmed_Idle/Unarmed_Idle_full.png")
+        self.sprite_sheet_idle = pygame.image.load("sprites/monatronica2000/PNG/Unarmed_Idle/Unarmed_Idle_full.png")
         self.sprite_sheet_run = pygame.image.load("sprites/mona/PNG/Unarmed_Run/Unarmed_Run_full.png")
         self.sprites = self.cut_sprites()
         self.sprite_idx = 0
